f3a5f22804d93edf0afbdc7f25d0c42dd2f4ad28
Killyan <killyan.faucon@hotmail.fr>
2023-11-15 12:51:32 +0100

    tghbj

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://mymoviz-backend-navy.vercel.app';
+const BACKEND_URL = 'https://mymoviz-backend-9c8wiwasd-mistpandoras-projects.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://mymoviz-frontend-lac-rho.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
